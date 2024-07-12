64086689ebb9503f14f2508ac8cc41b1ce6bceb0
Douglas Alves <douglas_dca@hotmail.com>
2024-07-12 15:08:58 -0300

    feat: set up cors to accept incoming requests from frontend address only

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createTrip } from "./routes/create-trip";
 import { confirmTrip } from "./routes/confirm-trip";
 
 const app = fastify();
+
+app.register(cors, {
+  origin: 'localhost:3000' // Front-end address  
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
