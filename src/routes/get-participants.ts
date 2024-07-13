57d9406acfa06b98ed70f2455afc7be49bc74e41
Douglas Alves <douglas_dca@hotmail.com>
2024-07-13 00:14:37 -0300

    feat: create participants GET route to list participants

---
+++ src/routes/get-participants.ts
@@ -0,0 +1,25 @@
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function getParticipants(app: FastifyInstance) {
+	app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/participants', {
+		schema: {
+      params: z.object({
+        tripId: z.string().uuid()
+      }),
+		}
+	}, async (request) => {
+    const { tripId } = request.params;
+
+    const trip = await prisma.trip.findUnique({
+      where: { id: tripId },
+      include: { participants: true }
+    });
+
+    if (!trip) throw new Error('Trip not found.');
+
+		return { participants: trip.participants };
+	});
+}
